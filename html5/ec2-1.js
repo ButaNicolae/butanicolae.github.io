d04f81c0d146d38ecaeb2467702f94c7c82f56f0
Black-Laptop <buta.nicolae997@yahoo.com>
2018-10-15 13:49:34 +0300

    .

---
+++ html5/ec2-1.js
@@ -0,0 +1,38 @@
+document.getElementById("id_bussiness_version").innerHTML= " Bussines_version : 2018.10.15.2";
+
+
+
+//------------------------------------------
+function citire()
+{
+
+
+
+}
+
+//------------------------------------------
+function rezolvare()
+{
+
+
+	
+}
+
+//------------------------------------------
+function afisare()
+{
+
+
+	
+}
+
+
+//------------------------------------------
+function rezolva()
+{
+
+	citire();
+	rezolvare();
+	afisare();
+	
+}
