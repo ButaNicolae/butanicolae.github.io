96e2f8a774ab460275e6a9495cbed212dd013b21
ButaNicolae <40632896+ButaNicolae@users.noreply.github.com>
2018-11-05 13:31:29 +0200

    Add files via upload

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,25 +1,31 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.2";
-
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.05.0";
 window.addEventListener("deviceorientation", on_device_orientation);
+window.addEventListener("devicemotion", on_device_motion);
 
-//------------------------------------------------------
+//--------------------------------
 function on_device_orientation(e)
 {
 	document.getElementById("id_alpha").innerHTML = e.alpha;
 	document.getElementById("id_beta").innerHTML = e.beta;
-	document.getElementById("id_gamma").innerHTML = e.gamma;
+	document.getElementById("id_gamma").innerHTML = e.gamma;	
 	
 	var canvas = document.getElementById("id_canvas");
 	var context = canvas.getContext("2d");
 	
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	
 	var R = 10;
 	context.beginPath();
-	context.arc(canvas.width / 2 + e.gamma / 90 * (canvas.width / 2 - R), 
+	context.arc(canvas.width / 2 + e.gamma / 90 *(canvas.width / 2-R), 
 				canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R), 
-				R, 
-				0, 2 * Math.PI);
+				R, 0, 2 * Math.PI);
 	context.stroke();
+	
+	
 }
-//------------------------------------------------------
+//--------------------------------
+function on_device_motion(e)
+{
+	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
+	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
+	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+}
