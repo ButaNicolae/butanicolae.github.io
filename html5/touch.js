818087b35463eb201bfd27449676bce2f638a3b1
ButaNicolae <40632896+ButaNicolae@users.noreply.github.com>
2018-11-26 15:33:27 +0200

    Add files via upload

--- html5/touch.js
+++ html5/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Logic version: 2018.11.26.8";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.10";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
@@ -14,10 +14,12 @@ function on_touch_start(e)
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
+		context.fillStyle = "black";
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 					e.changedTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0, 2 * Math.PI);
+		context.fill();
 		context.stroke();
 		var last_pos = {x: e.changedTouches[i].pageX, 
 						y: e.changedTouches[i].pageY, 
@@ -47,10 +49,12 @@ function on_touch_move(e)
 		
 		context.beginPath();
 		context.lineWidth = 1;		
+		context.fillStyle = "black";
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 					e.changedTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0, 2 * Math.PI);
+		context.fill();
 		context.stroke();
 		
 		last_pos_array[j].x = e.changedTouches[i].pageX;
