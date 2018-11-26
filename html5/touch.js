955d01e4da239156f0c51962b271625d77a84593
ButaNicolae <40632896+ButaNicolae@users.noreply.github.com>
2018-11-26 15:10:00 +0200

    Add files via upload

--- html5/touch.js
+++ html5/touch.js
@@ -1,73 +1,41 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.8";
-
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.26.11.6";
 var canvas = document.getElementById("id_canvas");
-canvas.addEventListener("touchstart", on_touch_start);
-canvas.addEventListener("touchmove", on_touch_move);
-canvas.addEventListener("touchmove", on_touch_end);
+canvas.addEventListener("touchstart",on_touch_start);
+canvas.addEventListener("touchmove",on_touch_move);
 
 var canvas_bounding_rect = canvas.getBoundingClientRect();
-
-
-var pozitii = [];
-//-------------------------------------------
+var last_pos = {x: 0, y: 0};
+//----------------------------------------------
 function on_touch_start(e)
 {
-	e.preventDefault();
-	
-	for (var i = 0; i < e.changedTouches.length; i++){
+	for (var i=0;i<e.changedTouches.length;i++)
+	{
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, 
-					e.changedTouches[i].pageY - canvas_bounding_rect.top,
-					10,
-					0,
-					2 * Math.PI);
+		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2*Math.PI);
 		context.stroke();
-		var last_pos = {x: e.changedTouches[i].pageX, y : e.changedTouches[i].pageY, id: e.changedTouches[i].identifier};
-		pozitii.push(last_pos);		
+		last_pos.x = e.changedTouches[i].pageX;
+		last_pos.y = e.changedTouches[i].pageY;
 	}
 }
-//-------------------------------------------
+//----------------------------------------------
 function on_touch_move(e)
 {
 	e.preventDefault();
-	
-	for (var i = 0; i < e.changedTouches.length; i++){
-		var j = 0;
-		for (; j < pozitii.length; j++)
-			if (pozitii[j].id == e.changedTouches[i].identifier)
-				break;
-			
+	for (var i=0;i<e.changedTouches.length;i++)
+	{
 		var context = canvas.getContext("2d");
 		context.beginPath();
 		context.lineWidth = 20;
-		context.moveTo(pozitii[j].x - canvas_bounding_rect.left, pozitii[j].y - canvas_bounding_rect.top);
-		context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left, 
-						e.changedTouches[i].pageY - canvas_bounding_rect.top);
+		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
+		context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top)
 		context.stroke();
-		
+		//-------------------
 		context.beginPath();
 		context.lineWidth = 1;
-		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, 
-					e.changedTouches[i].pageY - canvas_bounding_rect.top,
-					10,
-					0,
-					2 * Math.PI);
+		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2*Math.PI);
 		context.stroke();
-		pozitii[j].x = e.changedTouches[i].pageX;
-		pozitii[j].y = e.changedTouches[i].pageY;
-	}
-}
-//-------------------------------------------
-function on_touch_end(e)
-{
-	e.preventDefault();
-	
-	for (var i = 0; i < e.changedTouches.length; i++){
-		var j = 0;
-		for (; j < pozitii.length; j++)
-			if (pozitii[j].id == e.changedTouches[i].identifier)
-				break;
-		pozitii.splice(j, 1);
+		last_pos.x = e.changedTouches[i].pageX;
+		last_pos.y = e.changedTouches[i].pageY;
 	}
 }
